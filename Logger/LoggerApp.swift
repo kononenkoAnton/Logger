608468c418e9346a232fe04d254e251ac65efae0
kononenkoAnton <a.kononenko@applicaster.com>
2022-06-16 12:18:35 -0400

    chore: fix socket cancelation

--- Logger/LoggerApp.swift
+++ Logger/LoggerApp.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 @main
-struct LoggerApp: App {
+struct LoggerApp: App, ApplicationDelegateProtocol {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    let loggerViewModel: LoggerViewModel
+    func applicationShouldTerminate() {
+        loggerViewModel.stopLocalServer()
+    }
+
+    init() {
+        loggerViewModel = LoggerViewModel()
+        appDelegate.delegate = self
+    }
+
     var body: some Scene {
         WindowGroup {
-            let logger = LoggerViewModel()
-            MainLoggerView(loggerViewModel: logger)
+            MainLoggerView(loggerViewModel: loggerViewModel)
         }
     }
 }
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var delegate: LoggerApp!
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        delegate.applicationShouldTerminate()
+        return true
+    }
+}
+
+protocol ApplicationDelegateProtocol {
+    func applicationShouldTerminate()
+}
--- Logger/LoggerViewModel.swift
+++ Logger/LoggerViewModel.swift
@@ -16,8 +16,8 @@ class LoggerViewModel: ObservableObject {
 
     @Published var loggerModel: LoggerModel
     @Published var filteredEvents: [EventModel] = []
-    var wasFiltered = false
-    var localServer = LocalSever()
+    private var wasFiltered = false
+    private var localServer = LocalSever()
 
     init() {
         let strIPAddress: String = getIPAddress()
@@ -40,10 +40,6 @@ class LoggerViewModel: ObservableObject {
         }
     }
 
-    deinit {
-        localServer.stopLocalServer()
-    }
-
     // MARK: - Intents(s)
 
     var logLevel = UserDefaultManager.getLogLevel() {
@@ -64,6 +60,10 @@ class LoggerViewModel: ObservableObject {
             }
             prepareFilteredData()
         }
+    }
+
+    func stopLocalServer() {
+        localServer.stopLocalServer()
     }
 
     func clearLoggerData() {
