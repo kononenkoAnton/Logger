9f872d5b6bbad740082a2161e4c7fd617aa01152
kononenkoAnton <a.kononenko@applicaster.com>
2022-07-29 12:25:09 -0400

    fix: add caching logic for bottom detail view to prevent collapsing

--- Logger/Views/BottomDetailView/BottomDetailView.swift
+++ Logger/Views/BottomDetailView/BottomDetailView.swift
@@ -7,16 +7,34 @@
 
 import SwiftUI
 
+var cachedDetailsList: DetailsList?
+var cachedModel: EventModel?
+
 struct BottomDetailView: View {
     @ObservedObject var loggerViewModel: LoggerViewModel
     var body: some View {
-        VStack {
-            if let model = loggerViewModel.getModelSelected() {
-                DetailsList(items: loggerViewModel.dataModels(from: model))
-            }
+        return VStack {
+            getDetailListComponent()
         }.frame(maxWidth: .infinity,
                 maxHeight: .infinity)
             .background(Color(ColorKeys.BottomDetailViewColor))
+    }
+
+    
+    /// It returns new or cached details list component. It needed to prevent collapsing in case rerender same event model
+    func getDetailListComponent() -> DetailsList? {
+        if let model = loggerViewModel.getModelSelected() {
+            if let cachedModel = cachedModel,
+               cachedModel == model {
+            } else {
+                cachedDetailsList = DetailsList(items: loggerViewModel.dataModels(from: model))
+                cachedModel = model
+            }
+        } else {
+            cachedModel = nil
+            cachedDetailsList = nil
+        }
+        return cachedDetailsList
     }
 }
 
